760afc764122735c87e8a660bc98f1b2e92d4557
StarKRE <ardar.igor@mail.ru>
2018-06-07 04:33:18 +0300

    06.06.2018 Added custom regex #LOVE_AIO_&_C =)

--- sources/lang/methods/methodDefinition/AIOMethodDefinitionBuilder.c
+++ sources/lang/methods/methodDefinition/AIOMethodDefinitionBuilder.c
@@ -1,5 +1,6 @@
 #include <mem.h>
 #include <stdio.h>
+#include <ctype.h>
 #include "../../../../headers/lang/methods/methodDefinition/AIOMethodDefinition.h"
 #include "../../../../headers/lib/utils/StringUtils.h"
 
@@ -28,6 +29,13 @@ AIOMutableListOfString *getSourceCodeOfMethod(char *methodName, AIOMutableListOf
     return methodCode;
 }
 
+//While empty method:
+AIOMutableListOfAnnotations *getAnnotationsOfMethod(char *name, AIOMutableListOfString *sourceCode, int startIndex) {
+    AIOMutableListOfAnnotations* annotations;
+    createMutableListOfAnnotations(&annotations);
+    return annotations;
+}
+
 AIOMethodDefinition *buildAIOMethodDefinition(char *methodName, AIOMutableListOfString *sourceCode, int startIndex) {
     //Create the same method definition:
     AIOMethodDefinition *methodDefinition = calloc(1, sizeof(AIOMethodDefinition));
@@ -36,4 +44,64 @@ AIOMethodDefinition *buildAIOMethodDefinition(char *methodName, AIOMutableListOf
         printf("%s\n", getStringInMutableListByIndex(methodCode, i));
     }
     return methodDefinition;
+}
+
+int isDefaultOperations(const char *operation) {
+    if (strlen(operation) > 0) {
+        if (operation[0] == '+' || operation[0] == '*' || operation[0] == '&') {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int isTheShortestInTheSameObject(const char *operation) {
+    int length = strlen(operation);
+    if (length > 0) {
+        if (isalpha(operation[0]) && operation[length - 1] == '~') {
+            for (int i = 1; i < length - 1; ++i) {
+                if (!isalnum(operation[i])) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int isTheShortestInTheOtherObject(const char *operation) {
+    int length = strlen(operation);
+    if (length > 0) {
+        if (isalpha(operation[0]) && operation[length - 1] == '~') {
+            int wasDelimiter = -1;
+            int startMethodNameIndex = 0;
+            for (int i = 1; i < length - 1; ++i) {
+                if (operation[i] == '.') {
+                    //.@*
+                    if (i + 2 < length && operation[i + 1] == '@') {
+                        wasDelimiter = 0;
+                        startMethodNameIndex = i + 2;
+                        break;
+                    } else {
+                        perror("illegal aio line with method invocation");
+                    }
+                }
+                if (!isalnum(operation[i])) {
+                    return -1;
+                }
+            }
+            if (wasDelimiter == 0) {
+                for (int i = startMethodNameIndex; i < length; ++i) {
+                    if (!isalnum(operation[i])) {
+                        return -1;
+                    }
+                }
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
+    return -1;
 }
