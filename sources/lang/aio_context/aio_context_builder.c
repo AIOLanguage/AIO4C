47419dadb518fbb409958002fa37b85104b75676
StarKRE <ardar.igor@mail.ru>
2018-08-04 16:02:41 +0300

    Loading aio context is completed!

--- sources/lang/aio_context/aio_context_builder.c
+++ sources/lang/aio_context/aio_context_builder.c
@@ -14,13 +14,13 @@ aio_function_definition *delve_method_definition(const_string source_code, int *
 
 void collect_function_definitions_in_manager(aio_context *context) {
     const_string source_code = context->source_code;
+    const size_t source_code_length = strlen(source_code);
     aio_function_definition_map *definition_map = context->function_manager->function_definition_map;
-    const int code_line_length = strlen(source_code);
-    if (code_line_length == 0) {
+    if (is_empty_string(source_code)) {
         throw_error("aio context is empty!");
     }
     int pointer = 0;
-    while (pointer < code_line_length) {
+    while (pointer < source_code_length) {
         aio_function_definition *method_definition = delve_method_definition(source_code, &pointer);
         put_aio_method_definition_in_map(definition_map, method_definition);
     }
